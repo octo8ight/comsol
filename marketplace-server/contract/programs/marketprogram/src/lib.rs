36070866488e007184cacb7f0cceec0857c333e3
OctoZeus <viroz180237@gmail.com>
2024-03-22 10:21:15 -0700

    Update lib.rs

--- marketplace-server/contract/programs/marketprogram/src/lib.rs
+++ marketplace-server/contract/programs/marketprogram/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Token, TokenAccount, Transfer as SplTransfer};
-use solana_program::system_instruction
+use anchor_lang::solana_program::system_instruction;
 
 declare_id!("Ach3gA8LyvqD6YBxH5bSWZCxP45PaWtzPX8G2fYeYFbb");
 
@@ -8,7 +8,7 @@ declare_id!("Ach3gA8LyvqD6YBxH5bSWZCxP45PaWtzPX8G2fYeYFbb");
 pub mod marketprogram {
     use super::*;
 
-    pub fn acceptOffer(
+    pub fn acceptOffer( // transfer sol
         ctx: Context<Transferlamports>,
         amount: u64
     ) -> Result<()> {
@@ -30,6 +30,26 @@ pub mod marketprogram {
 
         Ok(())
     }
+
+    pub fn transfer_spl_tokens(ctx: Context<TransferSpl>, amount: u64) -> Result<()> { // transfer spl token
+        let destination = &ctx.accounts.to_ata;
+        let source = &ctx.accounts.from_ata;
+        let token_program = &ctx.accounts.token_program;
+        let authority = &ctx.accounts.from;
+
+        // Transfer tokens from taker to initializer
+        let cpi_accounts = SplTransfer {
+            from: source.to_account_info().clone(),
+            to: destination.to_account_info().clone(),
+            authority: authority.to_account_info().clone(),
+        };
+        let cpi_program = token_program.to_account_info();
+        
+        token::transfer(
+            CpiContext::new(cpi_program, cpi_accounts),
+            amount)?;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -37,6 +57,16 @@ pub struct Transferlamports<'info> {
     #[account(mut)]
     pub from: Signer<'info>,
     #[account(mut)]
-    pub to: AccountInfo<'info>,
+    pub to: AccountInfo<'info>,/// CHECK:
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct TransferSpl<'info> {
+    pub from: Signer<'info>,
+    #[account(mut)]
+    pub from_ata: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub to_ata: Account<'info, TokenAccount>,
+    pub token_program: Program<'info, Token>,
 }
