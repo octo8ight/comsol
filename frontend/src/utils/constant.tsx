0b840e67a3f296a437afa58d88dea52ccef0dac8
OctoZeus <viroz180237@gmail.com>
2024-04-01 02:10:10 -0700

    Update constant.tsx

--- frontend/src/utils/constant.tsx
+++ frontend/src/utils/constant.tsx
@@ -1,6 +1,6 @@
 // export const API_URL = "http://127.0.0.1:5000";
 // export const API_URL = "http://167.88.170.61:5000";
-export const API_URL = "https://167.88.171.25:5000";
+export const API_URL = "http://167.88.171.25:5000";
 
 export const OFFER_STATUS = {
     Waiting: 'waiting',
